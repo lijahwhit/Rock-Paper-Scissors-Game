0dc09c5bb079892620c99973cfd9f5f48cc3dcb9
lijahwhit <elijah@whitti.com>
2022-05-28 11:25:06 -0400

    /rps/-game/js/mail.js

--- js/main.js
+++ js/main.js
@@ -1,19 +1,16 @@
 import {game} from '/js/game.js';
 
-window.onload=function(){
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
 
-    const rock_div = document.getElementById("r");
-    const paper_div = document.getElementById("p");
-    const scissors_div = document.getElementById("s");
+function main() {
+    rock_div.addEventListener('click', () => game('r'));
 
-    function main() {
+    paper_div.addEventListener('click', () => game('p'));
+    
+    scissors_div.addEventListener('click', () => game('s'));
+}
 
-        rock_div.addEventListener('click', () => game('r'));
+main();
 
-        paper_div.addEventListener('click', () => game('p'));
-        
-        scissors_div.addEventListener('click', () => game('s'));
-    }
-
-    main();
-};
