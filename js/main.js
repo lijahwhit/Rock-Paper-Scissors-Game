d5084cc6313ea901187c2c519f5423c4ea922be0
lijahwhit <elijah@whitti.com>
2022-05-27 22:59:30 -0400

    test window.onload function

--- js/main.js
+++ js/main.js
@@ -1,16 +1,19 @@
 import {game} from '/js/game.js';
 
-const rock_div = document.getElementById("r");
-const paper_div = document.getElementById("p");
-const scissors_div = document.getElementById("s");
+window.onload=function(){
 
-function main() {
+    const rock_div = document.getElementById("r");
+    const paper_div = document.getElementById("p");
+    const scissors_div = document.getElementById("s");
 
-    rock_div.addEventListener('click', () => game('r'));
+    function main() {
 
-    paper_div.addEventListener('click', () => game('p'));
-    
-    scissors_div.addEventListener('click', () => game('s'));
+        rock_div.addEventListener('click', () => game('r'));
+
+        paper_div.addEventListener('click', () => game('p'));
+        
+        scissors_div.addEventListener('click', () => game('s'));
+    }
+
+    main();
 }
-
-main();
